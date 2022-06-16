8ec7984b3c97852781b669548c3d84f3d3bd817c
ckjets <ck_jets@yahoo.co.jp>
2022-06-16 22:04:07 +0900

    コメント削除

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Router } from 'express';
+import express from 'express';
 
 const app = express();
 const router = express.Router();
@@ -8,8 +8,6 @@ app.use(express.urlencoded({ extended: false }));
 
 /* GET home page. */
 router.get('/', (req: express.Request, res: express.Response, next) =>
-// renderで views配下を読み込んでる
-  // res.render('index', { title: 'Express' })
   res.send("hello world!!!!")
 );
 
@@ -18,30 +16,3 @@ app.use('/', router);
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
-
-
-// export default app;
-
-// import express from "express";
-// import * as bodyParser from "body-parser";
-
-// const app = express();
-// const router = express.Router();
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// router.get(
-//   "/",
-//   async (req: express.Request, res: express.Response): Promise<void> => {
-//     res.send("hello worldaaaaaa");
-//   }
-// );
-
-// app.use("/", router);
-
-// app.listen(3000, () => {
-//   console.log("listening on port 3000");
-// });
-
-// export default app;
