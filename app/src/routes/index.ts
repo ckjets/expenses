8daf30c489d59ec10503ef58e4abdabdc43b7e87
c.koide <>
2022-06-25 14:25:52 +0900

    - dotenv、line/bot-sdkを追加 - エントリポイントを bin/wwwにもどした - error.ejs追加

---
+++ app/src/routes/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import * as line from '@line/bot-sdk';
+
+var router = express.Router();
+
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
+};
+
+router.get('/', function(req: express.Request, res: express.Response, next) {
+    res.send('hello world!!!')
+});
+
+router.post('/webhook', line.middleware(config), (req, res) => {
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
+  });
+  
+  const client = new line.Client(config);
+  function handleEvent(event: any) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return Promise.resolve(null);
+    }
+  
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: event.message.text
+    });
+  }
+
+  
+module.exports = router;
