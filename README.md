4728d66863210a78ed0805bdf67192575210b14d
c.koide <>
2022-07-23 15:34:26 +0900

    test

--- README.md
+++ README.md
@@ -20,5 +20,5 @@ https://express-app-expenses.herokuapp.com/
 
 
 ## Links
-- 
+- [Heroku](https://dashboard.heroku.com/)
 - [LINEデベロッパー](https://developers.line.biz/)
--- app/app.ts
+++ app/app.ts
@@ -1,13 +1,31 @@
+import {
+  // main APIs
+  Client,
+  middleware,
+
+  // exceptions
+  JSONParseError,
+  SignatureValidationFailed,
+
+  // types
+  TemplateMessage,
+  WebhookEvent,
+} from "@line/bot-sdk";
+import { Request, Response } from 'express'
+// import { config } from './src/config/webhook'
 require('dotenv').config()
+
+const config = {
+  channelAccessToken: "KhNfVxAbQPo3JceU5mF4iw6bYbffkSyLrH7JYUHTuYZPm1wrfW77uv4zQULjt/8TK1NSllI0VqklfFDiOwbGmN5Tsyh37aczBc80y567cdMOOVnpPLw4gRje20uWJwj9G87qk5yz9qZwJHypRNmxfgdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "372beb645ad962210c72f4348646e488"
+};
 
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-// ルーティング
-var indexRouter = require(`${__dirname}/src/routes/index.ts`);
+const indexRouter = require(`${__dirname}/src/routes/index.ts`);
 
 var app = express();
 
@@ -19,17 +37,39 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'))); // Express がプロジェクトルートの /public ディレクトリにあるすべての静的ファイルを処理するようにするため追加
+// Express がプロジェクトルートの /public ディレクトリにあるすべての静的ファイルを処理するようにするため追加
+app.use(express.static(path.join(__dirname, 'public')));
 
+
+/** line webhook */
+app.post('/webhook', middleware(config), (req: Request, res: Response) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+const client = new Client(config);
+
+const handleEvent = (event: WebhookEvent) => {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  });
+}
+
+/** ルーティング */
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req: any, res: any, next: any) {
+app.use((req: Request, res: Response, next: any) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err: any, req: any, res: any, next: any) {
+app.use((err: any, req: Request, res: Response, next: any) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
