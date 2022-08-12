03311bca3cd3610b1a6b8b3fbb6dd093385a9a8a
c.koide <>
2022-08-12 16:34:21 +0900

    test

--- app/app.ts
+++ app/app.ts
@@ -15,6 +15,8 @@ import { Request, Response } from 'express'
 // import { config } from './src/config/webhook'
 require('dotenv').config()
 // const log4js = require("log4js")
+const https = require("https")
+
 
 const config = {
   channelAccessToken: "KhNfVxAbQPo3JceU5mF4iw6bYbffkSyLrH7JYUHTuYZPm1wrfW77uv4zQULjt/8TK1NSllI0VqklfFDiOwbGmN5Tsyh37aczBc80y567cdMOOVnpPLw4gRje20uWJwj9G87qk5yz9qZwJHypRNmxfgdB04t89/1O/w1cDnyilFU=",
@@ -46,29 +48,86 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 
 // /** line webhook */
+/** @see https://line.github.io/line-bot-sdk-nodejs/guide/webhook.html#error-handling */
 app.use(middleware(config))
-app.post('/webhook', (req: Request, res: Response) => {
-  console.log('===webhook===')
-  console.log('===req===')
-  console.log(req)
-  console.log('===res===')
-  console.log(res)
-  // Promise
-  //   .all(req.body.events.map(handleEvent))
-  //   .then((result) => res.json(result))
-  res.json(req.body.events) // req.body will be webhook event object
-})
-const client = new Client(config)
-const handleEvent = (event: WebhookEvent) => {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null)
-  }
 
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text
-  })
-}
+
+// app.post('/webhook', (req: Request, res: Response) => {
+//   console.log('===webhook===')
+//   console.log('===req===')
+//   console.log(req)
+//   console.log('===res===')
+//   console.log(res)
+//   // Promise
+//   //   .all(req.body.events.map(handleEvent))
+//   //   .then((result) => res.json(result))
+//   res.json(req.body.events) // req.body will be webhook event object
+// })
+// const client = new Client(config)
+// const handleEvent = (event: WebhookEvent) => {
+//   if (event.type !== 'message' || event.message.type !== 'text') {
+//     return Promise.resolve(null)
+//   }
+
+//   return client.replyMessage(event.replyToken, {
+//     type: 'text',
+//     text: event.message.text
+//   })
+// }
+
+
+app.post("/webhook", function(req, res) {
+  res.send("HTTP POST request sent to the webhook URL!")
+  // ユーザーがボットにメッセージを送った場合、返信メッセージを送る
+  if (req.body.events[0].type === "message") {
+    // 文字列化したメッセージデータ
+    const dataString = JSON.stringify({
+      replyToken: req.body.events[0].replyToken,
+      messages: [
+        {
+          "type": "text",
+          "text": "Hello, user"
+        },
+        {
+          "type": "text",
+          "text": "May I help you?"
+        }
+      ]
+    })
+
+    // リクエストヘッダー
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + config.channelAccessToken
+    }
+
+    // リクエストに渡すオプション
+    const webhookOptions = {
+      "hostname": "api.line.me",
+      "path": "/v2/bot/message/reply",
+      "method": "POST",
+      "headers": headers,
+      "body": dataString
+    }
+
+    // リクエストの定義
+    const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d)
+      })
+    })
+
+    // エラーをハンドル
+    request.on("error", (err) => {
+      console.error(err)
+    })
+
+    // データを送信
+    request.write(dataString)
+    request.end()
+  }
+})
+
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: any) => {
