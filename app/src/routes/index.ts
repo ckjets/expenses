0ea9444cee57b3dfc9e7ae41125c5d0ca624a940
c.koide <>
2022-09-17 18:18:02 +0900

    検証

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -12,23 +12,27 @@ router.get('/', function(req: Request, res: Response, next) {
     res.send('hello world!!!')
 });
 
-// router.post('/webhook', line.middleware(config), (req: Request, res: Response) => {
-//     Promise
-//       .all(req.body.events.map(handleEvent))
-//       .then((result) => res.json(result));
-//   });
+router.get('/test', function(req: Request, res: Response, next) {
+    res.send('test')
+});
+
+router.post('/webhook', line.middleware(config), (req: Request, res: Response) => {
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
+  });
   
-//   const client = new line.Client(config);
-//   function handleEvent(event: any) {
-//     if (event.type !== 'message' || event.message.type !== 'text') {
-//       return Promise.resolve(null);
-//     }
+  const client = new line.Client(config);
+  function handleEvent(event: any) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return Promise.resolve(null);
+    }
   
-//     return client.replyMessage(event.replyToken, {
-//       type: 'text',
-//       text: event.message.text
-//     });
-//   }
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: event.message.text
+    });
+  }
 
   
 module.exports = router;
