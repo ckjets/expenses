d153439aae8c5ed4fd8d457cfc2394e2874373ee
c.koide <>
2022-09-17 19:13:56 +0900

    シンプルな構成で疎通できるかテスト

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,4 @@
+import { middleware } from "@line/bot-sdk";
 require('dotenv').config()
 
 var createError = require('http-errors');
@@ -11,6 +12,11 @@ var indexRouter = require(`${__dirname}/src/routes/index.ts`);
 
 var app = express();
 
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN ||  "",
+  channelSecret: process.env.CHANNEL_SECRET || ""
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
@@ -22,6 +28,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); // Express がプロジェクトルートの /public ディレクトリにあるすべての静的ファイルを処理するようにするため追加
 
 app.use('/', indexRouter);
+
+
+// https://line.github.io/line-bot-sdk-nodejs/api-reference/middleware.html#usage
+// globally
+// app.use(middleware(config))
+
+// or directly with handler
+app.post('/webhook', middleware(config), (req: any, res: any) => {
+  req.body.events // webhook event objects
+  req.body.destination // user ID of the bot (optional)
+})
 
 // catch 404 and forward to error handler
 app.use(function(req: any, res: any, next: any) {
@@ -40,3 +57,4 @@ app.use(function(err: any, req: any, res: any, next: any) {
 });
 
 module.exports = app;
+
--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -8,6 +8,8 @@ const config = {
     channelSecret: process.env.CHANNEL_SECRET
 };
 
+console.log("config!!", config) // 2022-09-17: 値とれてる
+
 router.get('/', function(req: Request, res: Response, next) {
     res.send('hello world!!!')
 });
@@ -16,25 +18,26 @@ router.get('/test', function(req: Request, res: Response, next) {
     res.send('test')
 });
 
-router.post('/webhook', line.middleware(config), (req: Request, res: Response) => {
-    console.log("========================")
-    console.log("config", config)
-    Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result));
-  });
+// 2022-09-17: app.tsで定義してみたのでコメントアウト
+// router.post('/webhook', line.middleware(config), (req: Request, res: Response) => {
+//     console.log("========================")
+//     console.log("config", config)
+//     Promise
+//       .all(req.body.events.map(handleEvent))
+//       .then((result) => res.json(result));
+//   });
   
-  const client = new line.Client(config);
-  function handleEvent(event: any) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-      return Promise.resolve(null);
-    }
+//   const client = new line.Client(config);
+//   function handleEvent(event: any) {
+//     if (event.type !== 'message' || event.message.type !== 'text') {
+//       return Promise.resolve(null);
+//     }
   
-    return client.replyMessage(event.replyToken, {
-      type: 'text',
-      text: event.message.text
-    });
-  }
+//     return client.replyMessage(event.replyToken, {
+//       type: 'text',
+//       text: event.message.text
+//     });
+//   }
 
   
 module.exports = router;
